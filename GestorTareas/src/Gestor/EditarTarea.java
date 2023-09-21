1424c735607ae406a1015bf84a5dec016a3b19aa
PabloMontero04 <pablonumon@gmail.com>
2023-09-21 18:36:43 +0200

    Final betis

--- GestorTareas/src/Gestor/EditarTarea.java
+++ GestorTareas/src/Gestor/EditarTarea.java
@@ -1,4 +1,6 @@
 package Gestor;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
@@ -17,15 +19,23 @@ public class EditarTarea {
                 System.out.println("Ingrese la nueva descripción de la tarea:");
                 String nuevaDescripcion = scanner.nextLine();
 
-                System.out.println("Ingrese la nueva fecha de la tarea (en milisegundos desde Epoch):");
-                long nuevaFechaMillis = scanner.nextLong();
-               
-                tarea.setNombre(nuevoNombre);
-                tarea.setDescripcion(nuevaDescripcion);
-                tarea.setFecha(new Date(nuevaFechaMillis));
+                System.out.println("Ingrese la nueva fecha de la tarea (en formato dd/MM/yyyy):");
+                String nuevaFechaStr = scanner.nextLine();
 
-                System.out.println("Tarea editada con éxito.");
-                return;
+                // Convertir la fecha ingresada a un objeto Date
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                try {
+                    Date nuevaFecha = dateFormat.parse(nuevaFechaStr);
+                    tarea.setNombre(nuevoNombre);
+                    tarea.setDescripcion(nuevaDescripcion);
+                    tarea.setFecha(nuevaFecha);
+
+                    System.out.println("Tarea editada con éxito.");
+                    return;
+                } catch (ParseException e) {
+                    System.out.println("Formato de fecha incorrecto. Utilice dd/MM/yyyy.");
+                    return;
+                }
             }
         }
         System.out.println("No se encontró ninguna tarea con el nombre '" + nombreTarea + "'.");
